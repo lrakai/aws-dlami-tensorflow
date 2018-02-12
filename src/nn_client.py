64bc086a1f93fd8876f016316b31430d2f04513f
Logan Rakai <lmrakai@gmail.com>
2018-02-12 12:08:50 -0700

    Add more comments

--- src/nn_client.py
+++ src/nn_client.py
@@ -9,26 +9,31 @@ hostport = 'localhost:9000'
 
 def do_prediction(hostport):
 
-    # Create connection
-    host, port = hostport.split(':')
-    channel = implementations.insecure_channel(host, int(port))
-    stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+  # Create connection
+  host, port = hostport.split(':')
+  channel = implementations.insecure_channel(host, int(port))
+  stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
-    # Initialize a request
-    request = predict_pb2.PredictRequest()
-    request.model_spec.name = 'nn'
-    request.model_spec.signature_name = 'prediction'
+  # Initialize a request
+  request = predict_pb2.PredictRequest()
+  request.model_spec.name = 'nn'
+  request.model_spec.signature_name = 'prediction'
 
-    # Use evenly-spaced points for test data
-    tests = temp_data = np.array([range(-1, 6, 1)]).transpose().astype(
-        np.float32)
-    actual = np.sum(0.5 * temp_data + 2.5, 1)
-    request.inputs['input'].CopyFrom(
-        tf.contrib.util.make_tensor_proto(tests, shape=tests.shape))
+  # Use evenly-spaced points for test data
+  tests = temp_data = np.array([range(-1, 6, 1)]).transpose().astype(
+    np.float32)
 
-    # Get prediction from server
-    result = stub.Predict(request, 5.0)  # 5 second timeout
-    return result, actual
+  # Set the tests as the input for prediction
+  request.inputs['input'].CopyFrom(
+    tf.contrib.util.make_tensor_proto(tests, shape=tests.shape))
+
+  # Get prediction from server
+  result = stub.Predict(request, 5.0) # 5 second timeout
+
+  # Compare to noise-free actual values
+  actual = np.sum(0.5 * temp_data + 2.5, 1)
+
+  return result, actual
 
 
 prediction, actual = do_prediction(hostport)
